a044bdb15d3702e8bc8ad9d8c8c181b7aa17538c
ren1023 <renyou.1023@gmail.com>
2023-11-24 13:37:38 +0800

    增加備註

--- animal.php
+++ animal.php
@@ -4,7 +4,7 @@
 <?php
 
 
-class Animal1{
+class Animal{
 
 }
 
@@ -12,19 +12,19 @@ interface bark{
     function b();
 }
 
-class Dog extends Animal1 implements bark{
+class Dog extends Animal implements bark{
 
     function b(){
         return "汪";
     }
 }
-class Cat extends Animal1{
+class Cat extends Animal{
     function b(){
         return "喵";
     }
 }
 
-// 在同一個資料夾中，不能有同名的class. 
+// 在同一個資料夾中，不能有同名的class. 因為外掛intelephense的提示，但還是能執行。
 $dog=new Dog;
 $cat=new Cat;
 
